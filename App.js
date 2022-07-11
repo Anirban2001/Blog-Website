c95406d7bea5670ddf6406d827bc92eb47a51577
Anirban2001 <deyanirban988@gmail.com>
2022-07-11 15:42:22 +0530

    made backend using express

--- App.js
+++ App.js
@@ -1,32 +1,78 @@
 const express = require("express");
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 // express app
 const app = express();
 
-// register view engine
-app.set('view engine', 'ejs');
+//connect to mondodb
+// const dbURI = 'mongodb+srv://Anirban_2001:Babansonu2001@blogproject.sia4gcs.mongodb.net/?retryWrites=true&w=majority';
+// mongoose.connect(dbURI) //{useNewUrlParser: true, useUnifiedTopology: true}
+// .then((result) => app.listen(3000))
+// .catch((err) => console.log(err))
 
-// listen for request
-app.listen(3000);
+// register view engine is used to inject dynamic data into the webpage
+app.set('view engine', 'ejs');
+app.listen(3000)
+
+// app.use((req, res, next) =>{
+//     console.log('new request made:');
+//     console.log('host: ',req.hostname);
+//     console.log('path: ',req.path);
+//     console.log('method: ',req.method);
+//     next();
+// });
+
+// app.use((req, res, next) =>{
+//     console.log('in the next middleware');
+//     next();
+// });
+
+//middleware and static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+// mongoose and mongo sendbox routes
+// app.get('/add-blog', (req, res) =>{
+//     const blog = new Blog({
+//         title:'new blog 2',
+//         snippet: 'about my new blog',
+//         body: 'more about my new blog'
+//     });
+
+//     blog.save()
+//     .then((result) =>{
+//         res.send(result)
+//     })
+//     .catch((err) =>{
+//         console.log(err)
+//     })
+// })
+
 
 app.get('/', (req, res) => {
-
+    const blogs = [
+        {title: 'Anirban finds eggs', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Bunu finds stars', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Maa finds diamond', snippet:'Lorem ipsum dolor sit amet consectetur'},
+    ];
     // res.send('<h1>home page</h1>');
-    res.sendFile('./views/index.html',{root:__dirname});
+    res.render('index',{title:'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
 
     // res.send('<h1>about page</h1>');
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about',{title:'About'});
 });
 
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create',{title:'Create a new blog'})
 })
 
 // 404 page
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
-})
+    res.status(404).render('404', {title:'404'})
+});
