e2d306c0d85705fd67e9941106badb14294023d1
Anirban2001 <deyanirban988@gmail.com>
2022-07-19 02:08:20 +0530

    gitignore updated

--- Learning/lec1_2/main.js
+++
@@ -1,7 +0,0 @@
-const nums = [1,2,3,4,5];
-
-const total = nums.reduce((total1, num) => {
-    return total1+num
-})
-
-console.log(total)
--- Learning/lec1_2/people.js
+++
@@ -1,7 +0,0 @@
-const people = ['anirban','baban','sonu','betu','papa'];
-const ages = [21, 21, 21, 21]
-console.log(people);
-
-module.exports = {
-    people, ages
-};
